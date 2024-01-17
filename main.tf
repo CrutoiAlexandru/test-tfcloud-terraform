cc8c813f6c227620cbd3b111c7993035837ad298
Crutoi Alexandru <crutoi2001@gmail.com>
2024-01-17 23:25:57 +0200

    add budget

--- main.tf
+++ main.tf
@@ -10,5 +10,21 @@ terraform {
 }
 
 provider "aws" {
-  region  = "eu-central-1"
+  region = "eu-central-1"
+}
+
+resource "aws_budgets_budget" "cost" {
+  name         = "cost-monthly"
+  budget_type  = "COST"
+  limit_amount = "1"
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 80
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = ["crutoitesteurofins@gmail.com"]
+  }
 }
