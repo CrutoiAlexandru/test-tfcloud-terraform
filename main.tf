046542f18d5d4f4cabb076d3156ff79c411634e9
Crutoi Alexandru <crutoi2001@gmail.com>
2024-01-20 17:32:47 +0200

    test base image

--- main.tf
+++ main.tf
@@ -17,13 +17,13 @@ resource "aws_budgets_budget" "cost" {
 data "aws_ami" "windows-dotnet-final" {
   filter {
     name   = "name"
-    values = ["windows-dotnet-base"]
+    values = ["dotnet-windows-base"]
   }
   owners = ["self"]
 }
 
 resource "aws_instance" "my-ec2" {
-  ami           = "ami-0faab6bdbac9486fb"
+  ami           = data.aws_ami.windows-dotnet-final.id
   instance_type = "t2.micro"
   for_each      = toset(var.instance_count)
 
