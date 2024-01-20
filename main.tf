1abe9a24800036b76de5374b2ff8a10ec16ae35d
Crutoi Alexandru <crutoi2001@gmail.com>
2024-01-20 17:53:19 +0200

    add sg and  key pair

--- main.tf
+++ main.tf
@@ -22,10 +22,30 @@ data "aws_ami" "windows-dotnet-final" {
   owners = ["self"]
 }
 
+resource "aws_security_group" "rdp_sg" {
+  name        = "rdp-security-group"
+  description = "Allow inbound RDP traffic on port 3389"
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "windows" {
+  key_name   = "windows-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC16LnGcIvNljiVOTtnmgcuBPeytIHwF/67gbeJgiEqBRkNgtQb9391EZuShz8k+hb5PVE+kdWZN88w5HNu1yWDaiHe3p5zOrBi4LzEFmvhecJjOrVV52jz+D4wQi/PcWdW8S1gRvGvHSXujUKzLG7so09ZU308nS/bQbP1knh92ztohBwz+oCx2E5P9lt9INxBp2VFKzhhJn2baxgFExqfCXkGNNpLAz30Jk9LFdmHSHwlTquEBeADgOfUDqZSSwz9+esM0LYJWN7DRbXdOYTot/NVnxMF2M9rNwcDPV586Kwq5TIhLHr5+4fHF/mEUDgYTldVpl58TnTs99zIr0IQpAThh61JuS7LPWIYsfiUGRZEAd3SagieJHPWe1URf5inUXxI1/SXnYNgSziMoyZo3rPH+isPHCISbyYdOze9ntRlo279/NRKFCqD4ly/cMvlJru+1Gi9sYthpS9LdVX/0y/FsW6wyFOsbQBcgaoB2IehOOHQ54dMEgQwJwQwJ+s= ali@Alexandru-Vitalis-MacBook-Air.local"
+}
+
 resource "aws_instance" "my-ec2" {
-  ami           = data.aws_ami.windows-dotnet-final.id
-  instance_type = "t2.micro"
-  for_each      = toset(var.instance_count)
+  ami             = data.aws_ami.windows-dotnet-final.id
+  instance_type   = "t2.micro"
+  for_each        = toset(var.instance_count)
+  key_name        = aws_key_pair.windows.id
+  security_groups = [aws_security_group.rdp_sg.id]
+
 
   tags = {
     Name = "test-instance-${each.key}"
