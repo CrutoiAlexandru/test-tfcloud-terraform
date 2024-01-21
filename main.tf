43b6788cc3c043bfc739e527263b0f27368c6177
Crutoi Alexandru <crutoi2001@gmail.com>
2024-01-21 17:53:42 +0200

    update sg

--- main.tf
+++ main.tf
@@ -39,6 +39,20 @@ resource "aws_security_group" "rdp_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_key_pair" "windows" {
